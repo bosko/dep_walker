fd0ec631beaa848ede593e883ed2d76a6e9bbabc
Bosko Ivanisevic <bosko.ivanisevic@gmail.com>
2011-04-28 14:14:29 +0200

    First functional version
    Dependencies can be tested on one or all installed gems. Fat binary
    gems are still not handled correctly because gem finds missing Ruby
    dll if it is different from the version in which gem is used.

--- lib/dep_walker.rb
+++ lib/dep_walker.rb
@@ -6,8 +6,69 @@ module DepWalker
   
   VERSION = '1.0.0'
 
-  def dependencies(dll_path = "")
-    raw_dependencies(dll_path).map {|dep| dep.downcase}
+  def check_all
+    all_deps = {}
+    Gem.source_index.gems.each do |name,spec|
+      gem_deps = check(spec)
+      all_deps[name] = gem_deps unless gem_deps.empty?
+    end
+    
+    all_deps
+  end
+
+  def check(name_or_spec)
+    gem_deps = {}
+    spec = name_or_spec.is_a?(String) ? Gem.source_index.gems[name_or_spec] : name_or_spec
+    walk_deps(spec).each do |k,v|
+      gem_deps[k] = v[:not_found] unless v[:not_found].empty?
+    end
+    gem_deps
+  end
+  
+  def walk_deps(gem_spec)
+    dep_tree = {}
+    if gem_spec.is_a? Gem::Specification
+      dependencies(gem_spec).each do |k,v|
+        found = {}
+        not_found = []
+        v.each do |shlib|
+          if found[shlib].nil? || not_found.include?(shlib)
+            shlib_dir = find_shlib_dir(File.dirname(k), shlib)
+            if shlib_dir
+              found[shlib] = shlib_dir
+            else
+              not_found << shlib
+            end
+          end
+        end
+        dep_tree[k] = {:found => found, :not_found => not_found}
+      end
+    end
+    dep_tree
+  end
+
+  def dependencies(gem_spec)
+    deps = {}
+    Dir.glob(File.join(gem_spec.full_gem_path, "**/*")).select do |f|
+      f if [".so",".dll"].include? File.extname(f)
+    end.each do |ext|
+      deps[ext] = dll_dependencies(ext)
+    end
+    deps
+  end
+
+  private
+
+  def find_shlib_dir(init_path, shlib)
+    ENV["PATH"].split(File::PATH_SEPARATOR).unshift(init_path).each do |path|
+      return path if File.file?(File.join(path, shlib))
+    end
+    nil
+  end
+
+  def dll_dependencies(dll_path = "")
+    return [] unless ['.dll', '.so'].include? File.extname dll_path
+    
+    raw_dependencies(dll_path).map {|dep| dep.downcase}.uniq
   end
 end
-
--- test/test_dep_walker.rb
+++ test/test_dep_walker.rb
@@ -3,16 +3,20 @@ require "dep_walker"
 
 class TestDepWalker < Test::Unit::TestCase
   def test_no_argument
-    deps = DepWalker.dependencies
+    deps = DepWalker.dll_dependencies
     assert deps.is_a? Array
     assert_equal 0, deps.length
   end
 
   def test_dep_walker
     path = File.join(File.expand_path('../../lib/dep_walker', __FILE__), 'dep_walker.so')
-    deps = DepWalker.dependencies path
+    deps = DepWalker.dll_dependencies path
     assert_equal 4, deps.length
     assert deps.include? 'msvcrt.dll'
     assert deps.include? 'imagehlp.dll'
+  end
+
+  def test_wrong_extension
+    assert_equal 0, DepWalker.dll_dependencies("dummy.txt").length
   end
 end
